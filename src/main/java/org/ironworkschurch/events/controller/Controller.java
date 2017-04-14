a679ffa60d36883ae8e8e16568653f4be29436da
Ray Rishty <raymond.rishty@gmail.com>
2017-04-14 09:25:30 -0400

    Stream contents from RSS feed

--- src/main/java/org/ironworkschurch/events/controller/Controller.java
+++ src/main/java/org/ironworkschurch/events/controller/Controller.java
@@ -1,9 +1,15 @@
 package org.ironworkschurch.events.controller;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
 @RestController
 public class Controller {
@@ -11,7 +17,15 @@ public class Controller {
   private String eventsUrl;
 
   @RequestMapping(value = "/", produces = "text/html")
-  public String toHtml() {
-    return "<html><head></head><body>Hello <i>Iron Works</i>. Check out the <a href=\"" + eventsUrl + "\">events feed</a></body></html>";
+  public void toHtml(HttpServletResponse response) throws IOException {
+    URL url = new URL(eventsUrl);
+    try (InputStream inputStream = url.openStream();
+         OutputStream outputStream = response.getOutputStream();) {
+
+      response.setContentType("application/xml");
+
+      IOUtils.copy(inputStream, outputStream);
+      response.flushBuffer();
+    }
   }
 }
