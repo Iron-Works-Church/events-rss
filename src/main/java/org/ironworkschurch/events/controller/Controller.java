ba260d7244f1ec920fcc7133c9ff9da0dc1989fa
Ray Rishty <raymond.rishty@gmail.com>
2017-04-14 09:08:41 -0400

    Inject configurable url

--- src/main/java/org/ironworkschurch/events/controller/Controller.java
+++ src/main/java/org/ironworkschurch/events/controller/Controller.java
@@ -1,13 +1,17 @@
 package org.ironworkschurch.events.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+  @Value("${events-url}")
+  private String eventsUrl;
+
   @RequestMapping(value = "/", produces = "text/html")
   public String toHtml() {
-    return "<html><head></head><body>Hello <i>Iron Works</i></body></html>";
+    return "<html><head></head><body>Hello <i>Iron Works</i>. Check out the <a href=\"" + eventsUrl + "\">events feed</a></body></html>";
   }
 }
